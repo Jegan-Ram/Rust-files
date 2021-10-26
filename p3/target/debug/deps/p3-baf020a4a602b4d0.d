b94f98468359d7e7923f6dbe63080ab4e8793b97
Gamerx20003 <jeganramamurthyv@gmail.com>
2021-10-26 12:40:29 +0530

    basics

---
+++ p3/target/debug/deps/p3-baf020a4a602b4d0.d
@@ -0,0 +1,5 @@
+/home/jegan/Desktop/rust/programs/p3/target/debug/deps/p3-baf020a4a602b4d0: src/main.rs
+
+/home/jegan/Desktop/rust/programs/p3/target/debug/deps/p3-baf020a4a602b4d0.d: src/main.rs
+
+src/main.rs:
---
+++ p3/target/debug/p3.d
@@ -0,0 +1,1 @@
+/home/jegan/Desktop/rust/programs/p3/target/debug/p3: /home/jegan/Desktop/rust/programs/p3/src/main.rs
